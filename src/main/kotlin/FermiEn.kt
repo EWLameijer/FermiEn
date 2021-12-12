c5b86182791b8fc144aa480f19886486f5f2bd6a
EWLameijer <eric_wubbo@hotmail.com>
2021-12-12 12:30:01 +0100

    0.1.0 Added capability to read in questions and answers from file, and print them (fairly nicely) on screen.

--- src/main/kotlin/FermiEn.kt
+++ src/main/kotlin/FermiEn.kt
@@ -1,7 +1,53 @@
-const val version = "0.0.0"
+import java.io.File
+import kotlin.text.StringBuilder
+
+const val version = "0.1.0"
+
+// TODO: make it read from a settings-file
+const val inputFileName = "notes.txt"
+
+data class Entry(val question: String, val answer: String)
+
+fun String.toEntry(): Entry {
+    val (question, answer) = split('\t')
+    return Entry(question, answer)
+}
+
+fun String.toLines(): List<String> {
+    val lines = mutableListOf<String>()
+    var backSlashActivated = false
+    val currentLine = StringBuilder()
+    forEach {
+        if (backSlashActivated) {
+            when (it) {
+                '\\' -> currentLine.append('\\')
+                't' -> currentLine.append('\t')
+                'n' -> {
+                    lines.add(currentLine.toString())
+                    currentLine.clear()
+                }
+                else -> throw IllegalArgumentException("String.toLines(): \"\\$it\" is an unknown character.")
+            }
+            backSlashActivated = false
+        } else if (it == '\\') backSlashActivated = true
+        else currentLine.append(it)
+    }
+    lines.add(currentLine.toString())
+    return lines
+}
+
+fun String.prettyPrint() = toLines().forEach(::println)
 
 fun main() {
-    println("FermiEn version $version: Start")
+    println("FermiEn version $version: Start\n")
+    val entries = File(inputFileName).readLines().map { it.toEntry() }
+    entries.forEach {
+        println("Question: ")
+        it.question.prettyPrint()
+        println("Answer: ")
+        it.answer.prettyPrint()
+        println()
+    }
 }
 
 fun forTesting(x: Int) = x * x
